628ac5d747c857f69e093f99008a2b2e5ba76bc6
aliadil <aliadilg@gmail.com>
2018-01-01 22:46:01 +0500

    DotNet Loop and if statement

---
+++ DotNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divOf3 = 0;
+for(int i=1 ; i<21 ; i++){
+    if (i%3==0){
+        divOf3+=i;
+        }
+        }
+Console.WriteLine(divOf3);
+Console.ReadLine();
+        }
+    }
+}
