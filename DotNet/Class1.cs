f8822e073d3691a4cc2b3e215fdbb3ac2c35a62d
aliadil <aliadilg@gmail.com>
2018-01-02 01:17:38 +0500

    Fibo

---
+++ DotNet/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet
+{
+    class Class1
+    {
+                var fibonacciNumbers = new List<int>{1,1};
+                for (int i = 0 ; i<=17 ;i++){
+                var previous = fibonacciNumbers[fibonacciNumbers.Count-1];
+                var previous2= fibonacciNumbers[fibonacciNumbers.Count-2];
+
+                fibonacciNumbers.Add(previous+previous2);
+                }
+                foreach (var item in fibonacciNumbers){
+                    Console.WriteLine(item);
+                } 
+    }
+}
